a4144bf76bc214c4cd84cac81c5d2c5630a0017f
halisa <halisasaipulaa@gmail.com>
2023-02-07 23:57:20 -0800

    created ReviewRepository interface

--- src/main/java/dev/halisa/movies/Review.java
+++ src/main/java/dev/halisa/movies/Review.java
@@ -8,7 +8,7 @@ import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document(collection = "movies")
+@Document(collection = "reviews")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -16,5 +16,9 @@ public class Review {
     @Id
     private ObjectId id;
     private String body;
+
+    public Review(String body) {
+        this.body = body;
+    }
 }
 
