15a8c08206ff600903b9a6367dbfc3ac17e13c18
halisa <halisasaipulaa@gmail.com>
2023-02-08 11:59:23 -0800

    Added CORS configuration

--- src/main/java/dev/halisa/movies/MoviesApplication.java
+++ src/main/java/dev/halisa/movies/MoviesApplication.java
@@ -2,6 +2,9 @@ package dev.halisa.movies;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 @SpringBootApplication
@@ -9,6 +12,20 @@ public class MoviesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MoviesApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("*")
+						.allowedMethods("*")
+						.allowedHeaders("*")
+						.allowCredentials(false).maxAge(3600);
+			}
+		};
 	}
 
 
