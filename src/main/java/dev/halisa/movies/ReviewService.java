5bb751420d9b9eb14022d63b2cba72c74e947b1e
halisa <halisasaipulaa@gmail.com>
2023-02-08 00:15:13 -0800

    created MovieController class and fixed the issue wich couldn't add review to reviewIds

--- src/main/java/dev/halisa/movies/ReviewService.java
+++ src/main/java/dev/halisa/movies/ReviewService.java
@@ -21,7 +21,7 @@ public class ReviewService {
 
         mongoTemplate.update(Movie.class)
                 .matching(Criteria.where("imdbId").is(imdbId))
-                .apply(new Update().push("reviews").value(review))
+                .apply(new Update().push("reviewIds").value(review))
                 .first();
 
         return review;
